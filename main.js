4aba2ba7b4cb20c9fdf05eef83bb549e1500d7dd
karoliine jakovleva <karoliinejakovleva@gmail.com>
2023-10-11 16:25:44 +0300

    Add boxes

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ function write(text){
     process.stdout.write(text);
 }
 
-console.log(chalk.bgGreen.magenta('hello world'));
-console.log(chalk.underline('hello world'));
-console.log(chalk.italic('hello world'));
-console.log(chalk.strikethrough('hello world'));
-console.log(chalk.inverse('hello world'));
 
 console.log(boxen(chalk.bgGreen.magenta('hello world'), {borderStyle:'round'}))
+
+
+console.log(boxen( chalk.blueBright.green ('lol'), {backgroundColor:'white', height: 6, width: 10, borderStyle: 'singleDouble'}));
+console.log(boxen(chalk.bgCyanBright ('tere tulemast'), {padding: 2, margin: 1, borderStyle: 'double'}));
+console.log(boxen(chalk.bgBlueBright.bold ('tere tulemast'), {padding: 2, margin: 4, borderStyle: 'classic', titleAlignment: 'right'}));
+console.log(boxen(chalk.blue ('jakovleva'),  {title: 'karoliine', borderColor: 'redBright'}));
+
+console.log(boxen(chalk.whiteBright.red('viies kast'), {borderStyle:'bold', backgroundColor:'yellow', height: 6, width: 20, textAlignment: 'center'}))
