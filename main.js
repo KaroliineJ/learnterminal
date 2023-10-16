a73f4aa42aa3a45e5e5975f196b174e2bb910138
karoliine jakovleva <karoliinejakovleva@gmail.com>
2023-10-16 08:57:47 +0300

    blablabla

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ function write(text){
     process.stdout.write(text);
 }
 
-
-console.log(boxen(chalk.bgGreen.magenta('hello world'), {borderStyle:'round'}))
-
-
-console.log(boxen( chalk.blueBright.green ('lol'), {backgroundColor:'white', height: 6, width: 10, borderStyle: 'singleDouble'}));
-console.log(boxen(chalk.bgCyanBright ('tere tulemast'), {padding: 2, margin: 1, borderStyle: 'double'}));
-console.log(boxen(chalk.bgBlueBright.bold ('tere tulemast'), {padding: 2, margin: 4, borderStyle: 'classic', titleAlignment: 'right'}));
-console.log(boxen(chalk.blue ('jakovleva'),  {title: 'karoliine', borderColor: 'redBright'}));
-
-console.log(boxen(chalk.whiteBright.red('viies kast'), {borderStyle:'bold', backgroundColor:'yellow', height: 6, width: 20, textAlignment: 'center'}))
+// for(let i = 0; i<255;i++){
+//     write(chalk.rgb(i,i,i)('#'));
+// }
+// console.log(chalk.italic('hello'));
+let time = new Date().toLocaleTimeString();
+write(time);
+setTimeout(()=>) {
+    write('\x1B[8D'); // A up B down C right D left
+    write('MM22    ');
+}, 5000 );
