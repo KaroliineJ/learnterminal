dbb940a7b73f2b7a89f491d47d51d98905b8144d
karoliine jakovleva <karoliinejakovleva@gmail.com>
2023-10-18 14:26:44 +0300

    yargs

--- main.js
+++ main.js
@@ -1,39 +1,8 @@
-import inquirer from 'inquirer'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+const argv = yargs(hidenBin(process.argv)).argv;
 
-inquirer.prompt([
-  {
-    name: "greeting",
-    message: "What is your name?",
-    type: "input",
-  },
-  {
-    name: "colors",
-    message: "What's your favorite color?",
-    type: "list",
-    choices: ["black", "red", "blue", "yellow", "green", "pink"]
-  },
-  {
-    name: "age",
-    message: "How old are you?",
-    type: "number",
-  },
-  {
-    name: "hobbies",
-    message: "What are your hobbies?",
-    type: "checkbox",
-    choices: ["reading", "sports", "cooking", "traveling", "painting", "other"]
-  },
-  {
-    name: "confirm",
-    message: "Are you sure?",
-    type: "confirm",
-  },
-  {
-    name: "password",
-    message: "Enter your password",
-    type: "password",
-  }
-])
-.then(function (answers) {
-  console.log(answers);
-});
+console.log(argv);
+console.log(argv.value1);
+console.log(argv.value2);
+
